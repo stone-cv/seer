7ca0705649b069fe12cda729f73c235e3f61d7b4
Anna Ustimova <ustimova.a@gmail.com>
2023-12-18 16:44:20 +0300

    models updated

--- src/core/models.py
+++ src/core/models.py
@@ -23,6 +23,12 @@ class Event(Base):
     type_id: Mapped[int] = mapped_column(ForeignKey("event_types.id"))
     event_type: Mapped['EventType'] = relationship(back_populates="event")  # ?
 
+    camera_id: Mapped[int] = mapped_column(ForeignKey("cameras.id"))
+    camera: Mapped['Camera'] = relationship(back_populates="event")
+
+    camera_roi_id: Mapped[int] = mapped_column(ForeignKey("camera_rois.id"))
+    camera_roi: Mapped['RegionOfInterest'] = relationship(back_populates="event")
+
     start_time: Mapped[DateTime] = mapped_column(DateTime)  # timestamp?
     end_time: Mapped[DateTime] = mapped_column(DateTime)  # timestamp?
 
@@ -57,6 +63,8 @@ class Camera(Base):
     name: Mapped[str] = mapped_column(String(255))
     url: Mapped[str] = mapped_column(String(255))
 
+    event: Mapped['Event'] = relationship(back_populates="camera")
+
     # roi_id: Mapped[int] = mapped_column(ForeignKey("roi_zones.id"))
     # event_type: Mapped['EventType'] = relationship(back_populates="event")  # ?
 
@@ -69,6 +77,8 @@ class RegionOfInterest(Base):
     id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
     name: Mapped[str] = mapped_column(String(255))
     coords: Mapped[str] = mapped_column(String(255))
+
+    event: Mapped['Event'] = relationship(back_populates="camera_roi")
 
     # event: Mapped['Event'] = relationship(back_populates="event_type")  # ?
 

66c3f9cf512b06d50e57114042fb39ff3dcb5ad1
Anna Ustimova <ustimova.a@gmail.com>
2024-07-16 14:26:33 +0300

    minor logger upd

--- src/core/logger.py
+++ src/core/logger.py
@@ -6,20 +6,16 @@ from logging.handlers import TimedRotatingFileHandler
 
 import src.core.config as cfg
 
-# Create a logger
 logger = logging.getLogger("my_logger")
-
-# Create a file handler with daily rotation
 os.makedirs(cfg.log_dir, exist_ok=True)
+
 # log_file = f"{cfg.log_dir}/log_{datetime.date.today()}.log"
 log_file = f"{cfg.log_dir}/log"
 file_handler = TimedRotatingFileHandler(log_file, when="midnight", interval=1, backupCount=30)
 file_handler.namer = lambda name: name + ".log"
 
-# Create a stream handler for console logging
 console_handler = logging.StreamHandler()
 
-# Create a formatter
 formatter = logging.Formatter('%(asctime)s %(name)s:%(levelname)s - %(message)s')
 
 file_handler.setFormatter(formatter)
